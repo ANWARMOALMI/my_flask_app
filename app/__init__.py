6bcd79d6998c55502af2145a0a728c30a9e79ade
ANWARMOALMI <moalmi20142022@gmail.com>
2025-06-01 11:53:36 +0300

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+db = SQLAlchemy()
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
